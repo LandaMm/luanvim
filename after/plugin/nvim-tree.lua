062a5d6371603fff3c8870521a364309978526b5
LandaMm <qwer.009771@gmail.com>
2023-08-01 22:11:57 +0200

    feat: custom keymaps nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,12 +2,15 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local status, nvim_tree = pcall(require, 'nvim-tree')
+if (not status) then return end
+
+local function opts(desc)
+	return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+end
+
 local function my_on_attach(bufnr)
   local api = require "nvim-tree.api"
-
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
 
   -- default mappings
   api.config.mappings.default_on_attach(bufnr)
@@ -17,7 +20,11 @@ local function my_on_attach(bufnr)
   vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
 end
 
-require('nvim-tree').setup({
+nvim_tree.setup({
 	on_attach = my_on_attach,
 })
+
+vim.keymap.set('n', '<leader>tt', require('nvim-tree.actions.tree.toggle').fn, opts('Toggle'))
+vim.keymap.set('n', '<leader>tf', nvim_tree.focus, opts('Focus'))
+vim.keymap.set('n', '<leader>tc', require('nvim-tree.view').close, opts('Close'))
 
